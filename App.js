712574f95e28aa2ef34541efc05d3a55ee82b2ed
Pyronewbic <kan.nam.dev@gmail.com>
2021-03-01 13:43:23 +0530

    DF Integrated!

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
+import { Dialogflow_V2 } from 'react-native-dialogflow';
+import { DfConfig } from './env'
+
+const PSYCH_BOT = {
+  _id: 2,
+  name: 'pysch',
+  avatar: 'https://i.imgur.com/7k12EPD.png'
+}
 
 class App extends Component {
   state = {
@@ -9,18 +17,48 @@ class App extends Component {
         _id: 1,
         text: `Hey there - I'm your personal psychotherapist (Alpha Build).\n\nWhat's Up?`,
         createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'pysch',
-          avatar: 'https://i.imgur.com/7k12EPD.png'
-        }
+        user: PSYCH_BOT
       }
     ]
   };
 
+  componentDidMount() {
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_ENGLISH_US,
+      dialogflowConfig.project_id
+    );
+  }
+
+  handleGoogleResponse(result) {
+    let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+    this.sendBotResponse(text);
+  }
+
   onSend(messages = []) {
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages)
+    }));
+
+    let message = messages[0].text;
+    Dialogflow_V2.requestQuery(
+      message,
+      result => this.handleGoogleResponse(result),
+      error => console.log(error)
+    );
+  }
+
+  sendBotResponse(text) {
+    let msg = {
+      _id: this.state.messages.length + 1,
+      text,
+      createdAt: new Date(),
+      user: BOT_USER
+    };
+
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, [msg])
     }));
   }
 
