9f7cd74fb82bfe42b3bc1a9ed5abaebb3ce9fc5a
Pyronewbic <kan.nam.dev@gmail.com>
2021-03-01 14:07:06 +0530

    Touch up

--- App.js
+++ App.js
@@ -1,33 +1,34 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
 import { Dialogflow_V2 } from 'react-native-dialogflow';
-import { DfConfig } from './env'
 
-const PSYCH_BOT = {
+import { DfConfig } from './env';
+
+const PSYCH_USER = {
   _id: 2,
-  name: 'pysch',
+  name: 'FAQ Bot',
   avatar: 'https://i.imgur.com/7k12EPD.png'
-}
+};
 
 class App extends Component {
   state = {
     messages: [
       {
         _id: 1,
-        text: `Hey there - I'm your personal psychotherapist (Alpha Build).\n\nWhat's Up?`,
+        text: `Hey There - I'm your personal pyschotherapist (alpha).\n\nWhat's up?`,
         createdAt: new Date(),
-        user: PSYCH_BOT
+        user: PSYCH_USER
       }
     ]
   };
 
   componentDidMount() {
     Dialogflow_V2.setConfiguration(
-      dialogflowConfig.client_email,
-      dialogflowConfig.private_key,
+      DfConfig.client_email,
+      DfConfig.private_key,
       Dialogflow_V2.LANG_ENGLISH_US,
-      dialogflowConfig.project_id
+      DfConfig.project_id
     );
   }
 
@@ -54,7 +55,7 @@ class App extends Component {
       _id: this.state.messages.length + 1,
       text,
       createdAt: new Date(),
-      user: BOT_USER
+      user: PSYCH_USER
     };
 
     this.setState(previousState => ({
